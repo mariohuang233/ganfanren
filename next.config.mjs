c03b63679005b603c82339657465ef8477ff9f2c
Mario <mario@example.com>
2026-03-14 11:25:02 +0800

    Fix: 使用 standalone 模式适配 Zeabur

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
